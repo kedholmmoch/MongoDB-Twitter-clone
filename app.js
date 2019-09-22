f242729aa4c45f12a78051d0e7e027c053c4de99
kmoch <kevin.edholm.moch@gmail.com>
2019-09-22 15:28:03 -0700

    hmmm

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const port = process.env.PORT || 5000;
-const app = express();
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-  
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => res.send("Hello Outside World, again?"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
